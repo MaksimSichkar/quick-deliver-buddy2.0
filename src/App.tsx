d7e9d78c678f5c25afd9281d23cf9acb204cc30d
MaksimSichkar <147758725+MaksimSichkar@users.noreply.github.com>
2025-04-29 00:02:54 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import AddDeliveryPage from "./pages/AddDeliveryPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner position="top-center" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add-delivery" element={<AddDeliveryPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/index" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
